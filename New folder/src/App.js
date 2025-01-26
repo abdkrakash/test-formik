2f5aebb51f213c8fd83108a6bb34fc6f3c57289b
abdkarkash <abdkarkash@gmail.com>
2025-01-26 16:49:46 +0300

    done by all

--- New folder/src/App.js
+++ New folder/src/App.js
@@ -1,54 +1,15 @@
-import React from "react";
-function App() {
+import React from 'react'
+import Login from './components/login'
+import Signup from './components/signup'
 
+
+function App() {
   return (
     <div>
-      <section className="showcase">
-        <div className="overlay">
-         
-
-          <article>
-            <p className="tag">
-              <strong>Creat Your Account </strong> 
-            </p>
-
-            <form className="form">
-              <input
-                type="text"
-                name="firstName"
-                id="firstName"
-                placeholder="First Name"
-              />
-              <input
-                type="text"
-                name="lastName"
-                id="lastName"
-                placeholder="Last Name"
-              />
-
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email Address"
-              />
-              <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="Password"
-              />
-              <button type="sumbit">Sign up </button>
-              <small>
-                By clicking the button, you are agreeing to our{" "}
-                <span className="red">Terms and Services</span>
-              </small>
-            </form>
-          </article>
-        </div>
-      </section>
+      <Login />
+      <Signup />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
